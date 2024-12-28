37e05857ff0ee9ddf44ff85783589fe7a51a321e
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-28 00:21:18 -0300

    feat: add interceptor.

--- src/app/Interceptors/auth.interceptor.ts
+++ src/app/Interceptors/auth.interceptor.ts
@@ -1,5 +1,38 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Storage } from '@ionic/storage-angular';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private storage: Storage) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return from(this.storage.create()).pipe(
+      switchMap(() => {
+        return from(this.storage.get('jwt_token')).pipe(
+          switchMap((token: string | null) => {
+            if (token) {
+              const cloned = request.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              return next.handle(cloned);
+            }
+            return next.handle(request);
+          })
+        );
+      })
+    );
+  }
+}
