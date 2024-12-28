f312b3d2de03ed1af6582c12ce0fcc7e8aef2bf6
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-28 00:26:30 -0300

    feat: include routes for tabs.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Guards/Auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'tab1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tab1',
+    loadChildren: () =>
+      import('./tab1/tab1.module').then((m) => m.Tab1PageModule),
+  },
+  {
+    path: 'tab2',
+    loadChildren: () =>
+      import('./tab2/tab2.module').then((m) => m.Tab2PageModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'tab3',
+    loadChildren: () =>
+      import('./tab3/tab3.module').then((m) => m.Tab3PageModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'tab1',
+  },
 ];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
