e2a2d59266481b720ae6226592d4bfd9da94e6ff
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-31 00:46:07 -0300

    feat: add getUserOrders method.

--- src/app/Services/Order/order.service.ts
+++ src/app/Services/Order/order.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:5220/api/Order';
+
+  constructor(private http: HttpClient) {}
+
+  getUserOrders(pageNumber: number = 1, pageSize: number = 10): Observable<any> {
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber.toString())
+      .set('pageSize', pageSize.toString());
+      
+    return this.http.get<any>(this.baseUrl, { params });
+  }
 }
