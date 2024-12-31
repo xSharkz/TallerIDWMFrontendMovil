aeb69a1951b290ec5ac3c293698fd312b433b47f
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-31 01:54:08 -0300

    feat: add authentication visualization tabs.

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../Services/Auth/auth.service';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: false,
 })
-export class TabsPage {
-  constructor() {}
+export class TabsPage implements OnInit {
+
+  showTabs: boolean = false;  
+
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    const isAuthenticated = await this.authService.isAuthenticated(); 
+    this.showTabs = isAuthenticated;  
+  }
+
 }
