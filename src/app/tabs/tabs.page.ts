eacae87d62a298d161fc3f645a699eb82e61a810
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-31 00:46:42 -0300

    fix: change modules from tabs.

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,5 +6,4 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class TabsPage {
-  constructor(private router: Router) {}
 }
