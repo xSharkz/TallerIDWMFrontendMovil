f2c5305a2df1fd4fff10cde40c88ea315e3459fe
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-31 00:46:21 -0300

    feat: add orders visualization for tab2.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../Services/Order/order.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  orders: any[] = [];
+  loading: boolean = false;
 
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit() {
+    this.loadOrders();
+  }
+
+  loadOrders(pageNumber: number = 1, pageSize: number = 10) {
+    this.loading = true;
+    this.orderService.getUserOrders(pageNumber, pageSize).subscribe(
+      (response) => {
+        this.orders = response.orders; 
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error al cargar las órdenes:', error);
+        this.loading = false;
+      }
+    );
+  }
 }
