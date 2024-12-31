943137916a9f47112425f6e25991fd3792bfee80
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-12-31 01:09:08 -0300

    feat: add Product features.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../Services/Product/product.service';
+import { CartService } from '../Services/Cart/cart.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  products: any[] = [];
+  selectedType: string = '';
+  sortOrder: string = 'asc';
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  totalItems: number = 0;
+
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService
+      .getAvailableProducts(this.selectedType, this.sortOrder, this.pageNumber, this.pageSize)
+      .subscribe((response: any) => {
+        this.products = response.products;
+        this.totalItems = response.totalItems;
+      });
+  }
+
+  loadData(event: any) {
+    if (this.products.length < this.totalItems) {
+      this.pageNumber++;
+      this.getProducts();
+      event.target.complete();
+    } else {
+      event.target.disabled = true;
+    }
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+  }
 
 }
